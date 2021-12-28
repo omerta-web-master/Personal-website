3a1acf0ed44c2cf1f1b6b316e6d3490f2835a299
Dumitru <dumitrumerlusca@gmail.com>
2021-12-28 16:27:29 +0200

    Added geolocation api

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import initCanvasAnimation from "./canvas.js";
+const GEOLOCATION_API_KEY = "c77fbeed277844f6984cac026a86b03f";
+const BASE_API_URL = "https://portfolio-api123.herokuapp.com/";
 
 const canvas = document.querySelector("#canvas1");
 initCanvasAnimation(canvas);
@@ -33,4 +35,35 @@ initCanvasAnimation(canvas);
 			mobileNav.classList.remove("active");
 		});
 	});
+})();
+
+// Track users that visit this page
+(function trackUsers() {
+	document.addEventListener("DOMContentLoaded", async () => {
+		try {
+			const res = await fetch(
+				`https://api.ipgeolocation.io/ipgeo?apiKey=${GEOLOCATION_API_KEY}`
+			);
+			const data = await res.json();
+			addDataToDatabase(data);
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	async function addDataToDatabase(data) {
+		const data1 = data;
+		try {
+			const res = await fetch(`${BASE_API_URL}/api/visits`, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(data1),
+			});
+			const data = await res.json();
+		} catch (error) {
+			console.log(error);
+		}
+	}
 })();
